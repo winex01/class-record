d5571733e81f0b6a5a5fcd2fd71e9ca069682cb7
Winnie A. Damayo <winnie131212592@gmail.com>
2025-08-26 17:08:01 +0800

    remove welcome page

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -27,14 +27,14 @@ class AppPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('app')
-            ->path('app')
+            ->path('/')
             ->login()
             ->defaultThemeMode(ThemeMode::Dark)
             ->colors([
                 // 'primary' => Color::Amber,
                 'primary' => Color::Emerald,
                 'gray' => Color::Slate,
-                'stone' => Color::Stone,
+                // 'stone' => Color::Stone,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
