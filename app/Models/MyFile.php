9b156459b0d792e7696e3cd5b76b2cf3fd3ce46c
Winnie A. Damayo <winnie131212592@gmail.com>
2025-09-04 19:43:49 +0800

    remove files if deleted or edited

--- app/Models/MyFile.php
+++ app/Models/MyFile.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class MyFile extends Model
 {
@@ -12,4 +13,26 @@ class MyFile extends Model
         'files' => 'array',
         'tags' => 'array',
     ];
+
+    protected static function booted()
+    {
+        static::updating(function ($myFile) {
+            if ($myFile->isDirty('files')) {
+                $originalFiles = $myFile->getOriginal('files') ?? [];
+                $originalFiles = is_string($originalFiles) ? json_decode($originalFiles, true) : $originalFiles;
+
+                foreach ($originalFiles as $file) {
+                    if (! in_array($file, $myFile->files ?? [])) {
+                        Storage::delete($file);
+                    }
+                }
+            }
+        });
+
+        static::deleting(function ($myFile) {
+            foreach ($myFile->files ?? [] as $file) {
+                Storage::delete($file);
+            }
+        });
+    }
 }
