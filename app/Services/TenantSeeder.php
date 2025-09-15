23a8e93be986c962e7eb599356d2cfee37369949
Winnie A. Damayo <winnie131212592@gmail.com>
2025-09-15 16:28:21 +0800

    fix tenant seeder for assessment types

--- app/Services/TenantSeeder.php
+++ app/Services/TenantSeeder.php
@@ -13,12 +13,10 @@ class TenantSeeder
             ['name' => 'Quiz', 'user_id' => $user->id],
             ['name' => 'Test', 'user_id' => $user->id],
             ['name' => 'Exam', 'user_id' => $user->id],
-            ['name' => 'Assignment', 'user_id' => $user->id],
+            ['name' => 'Homework', 'user_id' => $user->id],
             ['name' => 'Project', 'user_id' => $user->id],
             ['name' => 'Oral Recitation', 'user_id' => $user->id],
             ['name' => 'Reporting', 'user_id' => $user->id],
-            ['name' => 'Presentation', 'user_id' => $user->id],
-            ['name' => 'Thesis', 'user_id' => $user->id],
         ];
 
         foreach ($types as $type) {
