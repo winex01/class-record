ca050448896aecc24796e73e33a587d299f5e4f2
Winnie A. Damayo <winnie131212592@gmail.com>
2025-09-29 18:53:14 +0800

    meeting

--- app/Filament/Widgets/MyCalendarWidget.php
+++ app/Filament/Widgets/MyCalendarWidget.php
@@ -2,21 +2,25 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Meeting;
+use Guava\Calendar\Enums\Context;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Builder;
 use Guava\Calendar\ValueObjects\FetchInfo;
 use Guava\Calendar\Filament\CalendarWidget;
+use Guava\Calendar\Contracts\ContextualInfo;
+use Guava\Calendar\ValueObjects\DateClickInfo;
+use Guava\Calendar\Filament\Actions\CreateAction;
 
 class MyCalendarWidget extends CalendarWidget
 {
     protected function getEvents(FetchInfo $info): Collection | array | Builder
     {
-        return [
-            \Guava\Calendar\ValueObjects\CalendarEvent::make()
-            ->title('My first calendar')
-            ->start(now())
-            ->end(now()->addHours(2)),
-        ];
+        return Meeting::query()
+        ->whereDate('ends_at', '>=', $info->start)
+        ->whereDate('starts_at', '<=', $info->end);
     }
+
+
 
 }
---
+++ app/Models/Meeting.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\BelongsToUser;
+use Guava\Calendar\Contracts\Eventable;
+use Illuminate\Database\Eloquent\Model;
+use Guava\Calendar\ValueObjects\CalendarEvent;
+
+class Meeting extends Model implements Eventable
+{
+    use BelongsToUser;
+    protected $guarded = [];
+
+    // This is where you map your model into a calendar object
+    public function toCalendarEvent(): CalendarEvent
+    {
+        // For eloquent models, make sure to pass the model to the constructor
+        return CalendarEvent::make($this)
+            ->title($this->name)
+            ->start($this->starts_at)
+            ->end($this->ends_at)
+            ;
+    }
+}
