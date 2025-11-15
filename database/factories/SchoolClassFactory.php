5aed2c3d390f5c1cf968e82aa65f8c1508e91956
Winnie A. Damayo <winnie131212592@gmail.com>
2025-11-15 09:14:54 +0800

    fix tags faker and use fruits

--- database/factories/SchoolClassFactory.php
+++ database/factories/SchoolClassFactory.php
@@ -22,12 +22,27 @@ class SchoolClassFactory extends Factory
             'Arts',
         ];
 
+
         return [
             'user_id'    => 1,
             'name'       => $this->faker->randomElement($subjects),
             'date_start' => $this->faker->optional()->date(),
             'date_end'   => $this->faker->optional()->date(),
-            'tags'       => $this->faker->words(3),
+            'tags' => $this->fruitTags(3),
+
         ];
+    }
+
+    public function fruitTags($count = 3)
+    {
+        $fruits = [
+            'Apple', 'Banana', 'Orange', 'Grape', 'Strawberry', 'Blueberry',
+            'Pineapple', 'Mango', 'Watermelon', 'Kiwi', 'Peach', 'Pear',
+            'Cherry', 'Raspberry', 'Blackberry', 'Lemon', 'Lime', 'Coconut',
+            'Pomegranate', 'Avocado', 'Fig', 'Date', 'Papaya', 'Guava',
+            'Lychee', 'Dragonfruit', 'Passionfruit', 'Apricot', 'Plum', 'Nectarine'
+        ];
+
+        return $this->faker->randomElements($fruits, $count);
     }
 }
