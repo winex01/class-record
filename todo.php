3ed82a27c62b6c0f5962ccffae43c35f635c7882
Winnie A. Damayo <winnie131212592@gmail.com>
2026-01-18 18:52:13 +0800

    todo

--- todo.php
+++ todo.php
@@ -4,3 +4,4 @@
 TODO:: import student
 TODO:: export student
 TODO:: clone class with checkbox to choose to copy: student, lessons, assessments.
+TODO:: fee collection -> make sure to add button to All Paid or make bulk action paid selected
