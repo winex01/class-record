bbdb93a3b139bf53993944c51a86e046285bdc33
Winnie A. Damayo <winnie131212592@gmail.com>
2026-01-24 11:11:04 +0800

    bug todo

--- todo.php
+++ todo.php
@@ -1,7 +1,8 @@
 <?php
 /*
 
-TODO:: bug in grade modal displaying grade if only has 1 component some of the column score dont have cell value
+TODO:: BUG: if you clone the class the assessment also showed in other class which shouln't happen because they are separate.
+        i think because we reuse just the same id of the assessment but not 100% sure.
 TODO:: in manage claass clusster menu page, add a box every page to display the details of the class or use the breadcrumbs
 TODO:: Grade Summary (Overall Grade) action modal
 TODO:: import student
