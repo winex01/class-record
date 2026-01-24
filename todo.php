2f9d2e85e558c8f29758320f22a3289120cb8287
Winnie A. Damayo <winnie131212592@gmail.com>
2026-01-24 10:45:18 +0800

    todo

--- todo.php
+++ todo.php
@@ -5,3 +5,4 @@ TODO:: import student
 TODO:: export student
 TODO:: TBD:: rename class tags into year and section
 TODO:: in manage claass clusster menu page, add a box every page to display the details of the class or use the breadcrumbs
+TODO:: Grade Summary (Overall Grade) action modal
