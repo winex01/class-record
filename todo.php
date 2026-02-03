d91bb6274ad516266e298687abb10d5447ac7354
Winnie A. Damayo <winnie131212592@gmail.com>
2026-02-03 22:10:34 +0800

    todo

--- todo.php
+++ todo.php
@@ -1,7 +1,7 @@
 <?php
 /*
-TODO:: attendance overview: add tab filter Perfect attendance and etc
-TODO:: attendance overiew: if possible in the modal when click the absent/present count open a modal and display those dates
+TODO:: attendance overiew: if possible in the modal when click the absent/present count open a modal and display those dates,
+        and when click those date it will open the attendance modal take attendance
 TODO:: student birthday notification in subnav Students
 TODO:: Grade Summary (Overall Grade) action modal
 TODO:: import student
