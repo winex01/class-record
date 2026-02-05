08d2a9fe9c40055c12345aa82f65d3397170354d
Winnie A. Damayo <winnie131212592@gmail.com>
2026-02-05 18:15:33 +0800

    center tab filter on overview modal

--- resources/views/livewire/attendance-overview-table.blade.php
+++ resources/views/livewire/attendance-overview-table.blade.php
@@ -1,27 +1,29 @@
 <div x-data="{ activeTab: $wire.entangle('activeTab').live }">
-    <x-filament::tabs>
-        <x-filament::tabs.item
-            alpine-active="activeTab === 'all'"
-            x-on:click="activeTab = 'all'"
-        >
-            All
-            <x-filament::badge>
-                {{ count($studentsData) }}
-            </x-filament::badge>
-        </x-filament::tabs.item>
+    <div class="flex justify-center mb-4">
+        <x-filament::tabs>
+            <x-filament::tabs.item
+                alpine-active="activeTab === 'all'"
+                x-on:click="activeTab = 'all'"
+            >
+                All
+                <x-filament::badge>
+                    {{ count($studentsData) }}
+                </x-filament::badge>
+            </x-filament::tabs.item>
 
-        <x-filament::tabs.item
-            alpine-active="activeTab === 'perfect'"
-            x-on:click="activeTab = 'perfect'"
-        >
-            Perfect Attendance
-            <x-filament::badge>
-                {{ count($perfectAttendanceData) }}
-            </x-filament::badge>
-        </x-filament::tabs.item>
-    </x-filament::tabs>
+            <x-filament::tabs.item
+                alpine-active="activeTab === 'perfect'"
+                x-on:click="activeTab = 'perfect'"
+            >
+                Perfect Attendance
+                <x-filament::badge>
+                    {{ count($perfectAttendanceData) }}
+                </x-filament::badge>
+            </x-filament::tabs.item>
+        </x-filament::tabs>
+    </div>
 
-    <div class="mt-4">
+    <div>
         {{ $this->table }}
     </div>
 </div>
