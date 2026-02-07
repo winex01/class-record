ff5f4f1cebaf5cb84b7b196253639f67df11503b
Winnie A. Damayo <winnie131212592@gmail.com>
2026-02-07 21:22:07 +0800

    todo

--- todo.php
+++ todo.php
@@ -1,6 +1,13 @@
 <?php
 /*
-TODO:: Grade Summary (Overall Grade) action modal
+TODO:: Assessment Overview:
+        * Display each student in modal and when click it will display all his assessment and his scores
+TODO:: Fee Collections Overview:
+        * Display each student in modal and total paid and also when click each collections and how much
+TODO:: Notification & calendar for:
+        * Lessons Completion date
+        * Assessments date
+        * Fee collections date
 TODO:: import student
 TODO:: export student
 TODO:: download active classs in excel
