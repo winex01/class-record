afbc641f033f408e04e4769b75936ae1cc82031b
Winnie A. Damayo <winnie131212592@gmail.com>
2026-02-11 19:24:20 +0800

    add tip on the modal, below the description of overview attendance

--- resources/views/filament/components/attendance-overview.blade.php
+++ resources/views/filament/components/attendance-overview.blade.php
@@ -1,3 +1,6 @@
+<div style="margin-top: -1.5rem;" class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+    <strong>Tip:</strong> Click on the present/absent counts to view the specific dates.
+</div>
 <div>
     @livewire('attendance-overview', ['schoolClassId' => $schoolClassId])
 </div>
