0ec0d4c1d23aac9189ec8db26adf32dbfe88db4a
Winnie A. Damayo <winnie131212592@gmail.com>
2026-02-17 11:20:11 +0800

    todo

--- todo.php
+++ todo.php
@@ -1,5 +1,6 @@
 <?php
 /*
+TODO:: in manage attendance table column absent/present display student absent/present
 TODO:: on every manage relation manager modal display the assessment details above the table like how the cluster sub page display the school class details
 
 TODO:: Notification & calendar for:
