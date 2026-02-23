5da9a3f29353a772929e018033459d7eca6d48f3
Winnie A. Damayo <winnie131212592@gmail.com>
2026-02-23 17:55:03 +0800

    todo

--- todo.php
+++ todo.php
@@ -1,6 +1,7 @@
 <?php
 /*
 TODO:: lessons overview / show all files to be downloaded
+TODO:: classess crud change from row to box, or grid style
 TODO:: on every manage relation manager modal display the assessment details above the table like how the cluster sub page display the school class details
 
 
@@ -11,11 +12,9 @@ TODO:: Notification & calendar for:
 
 TODO:: Grades: review computations on components and etc
 
-
 TODO:: table seeders: fee collections
-TODO:: check all tables column sortable/order
-TODO:: auto or remove the apply filters or apply columns.
 
+TODO:: check all tables column sortable/order
 TODO:: check all table search if there is an error, including the modals and tables.
 
 TODO:: import student
