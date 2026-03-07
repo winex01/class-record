a8cf686087b3d6ac357aac0de1fde00e009b91d8
Winnie A. Damayo <winnie131212592@gmail.com>
2026-03-07 13:16:25 +0800

    refactor manage assessment use convention

---
+++ app/Filament/Resources/SchoolClasses/Actions/SchoolClassAssessmentActions.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\SchoolClasses\Actions;
+
+use Filament\Actions\Action;
+use Filament\Support\Enums\Width;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Facades\Blade;
+use App\Filament\Resources\Students\StudentResource;
+use Guava\FilamentModalRelationManagers\Actions\RelationManagerAction;
+use App\Filament\Resources\SchoolClasses\RelationManagers\RecordScoreRelationManager;
+
+class SchoolClassAssessmentActions
+{
+    public static function overviewAction()
+    {
+        return
+            Action::make('overview')
+            ->color('info')
+            ->modalSubmitAction(false)
+            ->modalCancelAction(false)
+            ->modalWidth(Width::TwoExtraLarge)
+            ->modalHeading('Student Assessment Overview')
+            ->modalDescription(function ($livewire) {
+                return 'Overview of students across all assessment records for ' . $livewire->getOwnerRecord()->name;
+            })
+            ->modalContent(fn ($livewire) => new HtmlString(
+                Blade::render(
+                    '@livewire("assessment-overview", ["schoolClassId" => $schoolClassId])',
+                    ['schoolClassId' => $livewire->getOwnerRecord()->id]
+                )
+            ));
+    }
+
+    public static function recordScoreAction()
+    {
+        return
+            RelationManagerAction::make('recordScoreRelationManager')
+            ->label('Score')
+            ->icon(StudentResource::getNavigationIcon())
+            ->color('info')
+            ->slideOver()
+            ->relationManager(RecordScoreRelationManager::make())
+            ->modalDescription(fn ($record) => new HtmlString(
+                view('filament.components.assessment-modal-heading', [
+                    'record' => $record,
+                ])->render()
+            ));
+    }
+}
--- app/Filament/Resources/SchoolClasses/Pages/ManageSchoolClassAssessments.php
+++ app/Filament/Resources/SchoolClasses/Pages/ManageSchoolClassAssessments.php
@@ -3,25 +3,19 @@
 namespace App\Filament\Resources\SchoolClasses\Pages;
 
 use Filament\Tables\Table;
-use Filament\Actions\Action;
 use Filament\Schemas\Schema;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
-use Filament\Support\Enums\Width;
 use Filament\Actions\DeleteAction;
-use Illuminate\Support\HtmlString;
-use Illuminate\Support\Facades\Blade;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Resources\Pages\ManageRelatedRecords;
 use App\Filament\Traits\ManageSchoolClassInitTrait;
-use App\Filament\Resources\Students\StudentResource;
 use App\Filament\Resources\SchoolClasses\SchoolClassResource;
 use App\Filament\Resources\SchoolClasses\Actions\SchoolClassActions;
-use Guava\FilamentModalRelationManagers\Actions\RelationManagerAction;
 use App\Filament\Resources\SchoolClasses\Forms\SchoolClassAssessmentForm;
+use App\Filament\Resources\SchoolClasses\Actions\SchoolClassAssessmentActions;
 use App\Filament\Resources\SchoolClasses\Filters\SchoolClassAssessmentFilters;
 use App\Filament\Resources\SchoolClasses\Colulmns\SchoolClassAssessmentColumns;
-use App\Filament\Resources\SchoolClasses\RelationManagers\RecordScoreRelationManager;
 
 class ManageSchoolClassAssessments extends ManageRelatedRecords
 {
@@ -50,42 +44,14 @@ class ManageSchoolClassAssessments extends ManageRelatedRecords
             ->columns(SchoolClassAssessmentColumns::schema())
             ->filters([SchoolClassAssessmentFilters::types()])
             ->recordActions([
-                RelationManagerAction::make('recordScoreRelationManager')
-                    ->label('Score')
-                    ->icon(StudentResource::getNavigationIcon())
-                    ->color('info')
-                    ->slideOver()
-                    ->relationManager(RecordScoreRelationManager::make())
-                    ->modalDescription(fn ($record) => new HtmlString(
-                        view('filament.components.assessment-modal-heading', [
-                            'record' => $record,
-                        ])->render()
-                    )),
-
+                SchoolClassAssessmentActions::recordScoreAction(),
                 ViewAction::make(),
                 EditAction::make(),
                 DeleteAction::make(),
             ])
             ->toolbarActions([
-                SchoolClassActions::createWithStudentsAction($this->getOwnerRecord())
-                    ->label('New Assessment'),
-
-                Action::make('overview')
-                    ->color('info')
-                    ->modalSubmitAction(false)
-                    ->modalCancelAction(false)
-                    ->modalWidth(Width::TwoExtraLarge)
-                    ->modalHeading('Student Assessment Overview')
-                    ->modalDescription(function ($livewire) {
-                        return 'Overview of students across all assessment records for ' . $livewire->getOwnerRecord()->name;
-                    })
-                    ->modalContent(fn ($livewire) => new HtmlString(
-                        Blade::render(
-                            '@livewire("assessment-overview", ["schoolClassId" => $schoolClassId])',
-                            ['schoolClassId' => $livewire->getOwnerRecord()->id]
-                        )
-                    )),
-
+                SchoolClassActions::createWithStudentsAction($this->getOwnerRecord())->label('New Assessment'),
+                SchoolClassAssessmentActions::overviewAction(),
                 DeleteBulkAction::make(),
             ])
             ->recordAction('recordScoreRelationManager');
