e6abbfd694821197239e8465757aac840aec6924
Winnie A. Damayo <winnie131212592@gmail.com>
2026-03-08 22:34:53 +0800

    remove import unused

--- app/Filament/Resources/SchoolClasses/Pages/ManageSchoolClassGrades.php
+++ app/Filament/Resources/SchoolClasses/Pages/ManageSchoolClassGrades.php
@@ -16,7 +16,6 @@ use App\Filament\Columns\TextColumn;
 use Filament\Support\Enums\TextSize;
 use Filament\Actions\DeleteBulkAction;
 use Illuminate\Database\Eloquent\Builder;
-use App\Filament\Columns\BooleanIconColumn;
 use Filament\Resources\Pages\ManageRelatedRecords;
 use App\Filament\Traits\ManageSchoolClassInitTrait;
 use App\Filament\Resources\SchoolClasses\SchoolClassResource;
