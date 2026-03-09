637842b56eff78ce309c488157fc4702dd4fc5c9
Winnie A. Damayo <winnie131212592@gmail.com>
2026-03-10 06:59:24 +0800

    update todo

--- todo.php
+++ todo.php
@@ -1,26 +1,23 @@
 <?php
 /*
+TODO:: add Widgets in manage resources:
+    -students:: Display last 5 birthdays and 5 coming birthdays or TBD shotuld i put it as Overview modal?
+    -attendances::
+    -lessons:: completion dates alert
+    -assessments:: upcoming dates alert
+    -fee collections:: upcoming dates alert
 TODO:: grade overview
-
-TODO:: add widgets in ManageStudents:: TBD::ex. What's the activities today or Note or message to display if there are assessment, or lessons deadline incoming or etc.
-        * Lessons Completion date
-        * Assessments date
-        * Fee collections date
-
-TODO:: find a way to that the attendance will be converted to assessment if they want
-
 TODO:: Grades: review computations on components and etc
-
 TODO:: table seeders: fee collections
-
-TODO:: check all tables column sortable/order
 TODO:: check all table search if there is an error, including the modals and tables.
-
+TODO:: check all tables column sortable/order
 TODO:: import student
 TODO:: export student
 TODO:: download active classs in excel
 TODO:: download app backup
+TODO:: licensed Payment
 
+--END
 
 Architecture convention
 Forms
