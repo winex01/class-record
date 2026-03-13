a8157e22842e834871f8107c679a448d49b8cc3f
Winnie A. Damayo <winnie131212592@gmail.com>
2026-03-14 00:07:03 +0800

    todo

--- todo.php
+++ todo.php
@@ -1,5 +1,6 @@
 <?php
 /*
+TODO:: in grade action follow the order of components same in the Grading Settings action or ViewAction
 TODO:: manage class grade add modal description in Final Grades action
 TODO:: remove split columns in school class table
 TODO:: table seeders: fee collections
