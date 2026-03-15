570f3445cf67f5de157e731b33cc5803604f5c85
Winnie A. Damayo <winnie131212592@gmail.com>
2026-03-15 15:02:02 +0800

    DateColumn default toggle

--- app/Filament/Columns/DateColumn.php
+++ app/Filament/Columns/DateColumn.php
@@ -12,6 +12,7 @@ class DateColumn extends TextColumn
         parent::setUp();
 
         $this
+            ->toggleable(isToggledHiddenByDefault: false)
             ->label(fn ($column): string => Str::headline($column->getName()))
             ->wrap()
             ->date()
