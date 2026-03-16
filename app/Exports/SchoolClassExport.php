bf306f8480dc10cf3acb0b6fdc6f308d218c2ab6
Winnie A. Damayo <winnie131212592@gmail.com>
2026-03-16 12:34:52 +0800

    add gender styling

--- app/Exports/SchoolClassExport.php
+++ app/Exports/SchoolClassExport.php
@@ -3,13 +3,15 @@
 namespace App\Exports;
 
 use App\Models\SchoolClass;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class SchoolClassExport implements FromCollection, WithHeadings, WithStyles, ShouldAutoSize
+class SchoolClassExport implements FromCollection, WithHeadings, WithStyles, ShouldAutoSize, WithEvents
 {
     protected array $columnMap;
 
@@ -56,6 +58,39 @@ class SchoolClassExport implements FromCollection, WithHeadings, WithStyles, Sho
                     'startColor' => ['rgb' => '10B981'], // emerald-500
                 ],
             ],
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet->getDelegate();
+                $highestRow = $sheet->getHighestRow();
+
+                // find gender column index
+                $headings = $this->headings();
+                $genderCol = array_search('Gender', $headings);
+
+                if ($genderCol === false) return;
+
+                $genderColLetter = chr(ord('A') + $genderCol);
+
+                for ($row = 2; $row <= $highestRow; $row++) {
+                    $value = $sheet->getCell("{$genderColLetter}{$row}")->getValue();
+
+                    $color = match ($value) {
+                        'Male'   => '3B82F6', // blue/info
+                        'Female' => 'EC4899', // pink
+                        default  => '000000',
+                    };
+
+                    $sheet->getStyle("{$genderColLetter}{$row}")
+                        ->getFont()
+                        ->getColor()
+                        ->setARGB($color);
+                }
+            },
         ];
     }
 }
