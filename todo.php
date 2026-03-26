430428e868be254829e109b31aff2f9cb1f942f9
Winnie A. Damayo <winnie131212592@gmail.com>
2026-03-26 14:32:06 +0800

    update todo

--- todo.php
+++ todo.php
@@ -2,7 +2,6 @@
 /*
 TODO:: wrap this into .exe bundle
 
-TODO:: Grades: review computations on components and etc
 TODO:: check all table search if there is an error, including the modals and tables.
 TODO:: check all tables column sortable/order
 
